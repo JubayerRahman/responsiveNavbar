3953107573c90c532698889ea6b5cb8000704513
JubayerRahman <68602999+JubayerRahman@users.noreply.github.com>
2022-11-18 14:43:05 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+const hanburger = document.querySelector('.hanburger');
+const navlink = document.querySelector('.nav-links');
+
+hanburger.addEventListener('click',()=>{
+    navlink.classList.toggle('open')
+})
